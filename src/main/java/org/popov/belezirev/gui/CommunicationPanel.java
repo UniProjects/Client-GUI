15adc500f74623de26de0793b69875747970859a
Belezirev <encho.belezirev@sap.com>
2017-06-24 23:20:47 +0300

    Fix validation of user credentials

--- src/main/java/org/popov/belezirev/gui/CommunicationPanel.java
+++ src/main/java/org/popov/belezirev/gui/CommunicationPanel.java
@@ -28,189 +28,190 @@ import org.popov.belezirev.gui.client.ClientGui;
 import org.popov.belezirev.gui.client.MessageProcessor;
 
 public class CommunicationPanel extends JPanel {
-	private static final int SIGN_IN_PANEL_COMPONETS_SEPARATOR_WIDTH = 0;
+    private static final int SIGN_IN_PANEL_COMPONETS_SEPARATOR_WIDTH = 0;
 
-	private static final int SIGN_IN_PANEL_COMPONETS_SEPARATOR_HEIGHT = 15;
+    private static final int SIGN_IN_PANEL_COMPONETS_SEPARATOR_HEIGHT = 15;
 
-	private static final int DEFAULT_INPUT_AREA_WIDTH = 25;
+    private static final int DEFAULT_INPUT_AREA_WIDTH = 25;
 
-	private static final int DEFAULT_DISPLAY_LIST_WIDTH = 60;
+    private static final int DEFAULT_DISPLAY_LIST_WIDTH = 60;
 
-	private static final int OK_BUTTON = 0;
+    private static final int OK_BUTTON = 0;
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private static final String DEFAULT_TEXTAREA_VALUE = "";
-	private static final String USERS_PANEL_TITLE = "Online Users";
-	private static final int TOP_MARGIN_OFFSET = 0;
-	private static final int LEFT_MARGIN_OFFSET = 5;
-	private static final int BOTTOM_MARGIN_OFFSET = 0;
-	private static final int RIGHT_MARGIN_OFFSET = 15;
+    private static final String DEFAULT_TEXTAREA_VALUE = "";
+    private static final String USERS_PANEL_TITLE = "Online Users";
+    private static final int TOP_MARGIN_OFFSET = 0;
+    private static final int LEFT_MARGIN_OFFSET = 5;
+    private static final int BOTTOM_MARGIN_OFFSET = 0;
+    private static final int RIGHT_MARGIN_OFFSET = 15;
 
-	private JTextArea chatTextArea;
-	private JTextField textLine;
-	private JButton sendButton;
-	private JScrollPane textAreaScroller;
-	private ClientGui client;
-	private DefaultListModel<String> model;
+    private JTextArea chatTextArea;
+    private JTextField textLine;
+    private JButton sendButton;
+    private JScrollPane textAreaScroller;
+    private ClientGui client;
+    private DefaultListModel<String> model;
 
-	public CommunicationPanel() {
-	}
+    public CommunicationPanel() {
+    }
 
-	public void createClient() {
-		try {
-			while (true) {
-				client = new ClientGui("localhost", 10513, new MessageProcessor(chatTextArea));
-				client.initClient();
-				Credentials<String, String> credentials = getClientCredentials();
-				if (credentials == null) {
-					client.close();
-					continue;
-				}
-				client.sendMessage(credentials.getUsername());
-				client.sendMessage(credentials.getPassword());
-				if (isUsernameAcknowledged()) {
-					break;
-				}
-			}
-			client.sendMessage("gui");
-			String userNamesJoined = client.readMessageSynchronously();
-			List<String> userNames = parse(userNamesJoined);
-			addToDisplayList(userNames);
-			client.readMessageAsynchronously();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    public void createClient() {
+        try {
+            while (true) {
+                client = new ClientGui("localhost", 10513, new MessageProcessor(chatTextArea));
+                Credentials<String, String> credentials = getClientCredentials();
+                if (credentials == null) {
+                    continue;
+                }
+                client.initClient();
+                client.sendMessage(credentials.getUsername());
+                client.sendMessage(credentials.getPassword());
+                if (isUsernameAcknowledged()) {
+                    break;
+                }
+            }
+            client.sendMessage("gui");
+            String userNamesJoined = client.readMessageSynchronously();
+            List<String> userNames = parse(userNamesJoined);
+            addToDisplayList(userNames);
+            client.readMessageAsynchronously();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	private List<String> parse(String userNamesJoined) {
-		return Arrays.asList(userNamesJoined.split(","));
-	}
+    private List<String> parse(String userNamesJoined) {
+        return Arrays.asList(userNamesJoined.split(","));
+    }
 
-	private void addToDisplayList(List<String> usernames) {
-		for (String userName : usernames) {
-			model.addElement(userName);
-		}
-	}
+    private void addToDisplayList(List<String> usernames) {
+        for (String userName : usernames) {
+            model.addElement(userName);
+        }
+    }
 
-	private Credentials<String, String> getClientCredentials() {
-		final String[] options = { "SIGN-IN" };
-		final JPanel panel = new JPanel();
-		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-		final JLabel usernameAreaLabel = new JLabel("Enter your username: ");
-		final JLabel passwordAreaLabel = new JLabel("Enter your password: ");
-		final JTextField usernameArea = new JTextField(DEFAULT_INPUT_AREA_WIDTH);
-		final JPasswordField passwordArea = new JPasswordField(DEFAULT_DISPLAY_LIST_WIDTH);
-		panel.add(usernameAreaLabel);
-		panel.add(usernameArea);
-		panel.add(Box.createRigidArea(
-				new Dimension(SIGN_IN_PANEL_COMPONETS_SEPARATOR_WIDTH, SIGN_IN_PANEL_COMPONETS_SEPARATOR_HEIGHT)));
-		panel.add(passwordAreaLabel);
-		panel.add(passwordArea);
-		final int selectedOption = JOptionPane.showOptionDialog(null, panel, "SIGN-IN", JOptionPane.NO_OPTION,
-				JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
-		final String username = usernameArea.getText();
-		final String password = passwordArea.getPassword().toString();
-		if (selectedOption == OK_BUTTON && isCredentialValid(username) && isCredentialValid(password)) {
-			return new Credentials<>(username, password);
-		}
-		if (selectedOption == JOptionPane.CLOSED_OPTION) {
-			System.exit(0);
-		}
-		return null;
-	}
+    private Credentials<String, String> getClientCredentials() {
+        final String[] options = { "SIGN-IN" };
+        final JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        final JLabel usernameAreaLabel = new JLabel("Enter your username: ");
+        final JLabel passwordAreaLabel = new JLabel("Enter your password: ");
+        final JTextField usernameArea = new JTextField(DEFAULT_INPUT_AREA_WIDTH);
+        final JPasswordField passwordArea = new JPasswordField(DEFAULT_DISPLAY_LIST_WIDTH);
+        panel.add(usernameAreaLabel);
+        panel.add(usernameArea);
+        panel.add(Box.createRigidArea(new Dimension(SIGN_IN_PANEL_COMPONETS_SEPARATOR_WIDTH, SIGN_IN_PANEL_COMPONETS_SEPARATOR_HEIGHT)));
+        panel.add(passwordAreaLabel);
+        panel.add(passwordArea);
+        final int selectedOption = JOptionPane.showOptionDialog(null, panel, "SIGN-IN", JOptionPane.NO_OPTION, JOptionPane.PLAIN_MESSAGE,
+            null, options, options[0]);
+        final String username = usernameArea.getText();
+        final char[] password = passwordArea.getPassword();
+        if (selectedOption == OK_BUTTON && isUserNameValid(username) && isCredentialValidPassword(password)) {
+            return new Credentials<>(username, password.toString());
+        }
+        if (selectedOption == JOptionPane.CLOSED_OPTION) {
+            System.exit(0);
+        }
+        return null;
+    }
 
-	private Boolean isUsernameAcknowledged() {
-		return "valid_username".equals(client.readMessageSynchronously());
-	}
+    private boolean isCredentialValidPassword(char[] password) {
+        return password.length != 0;
+    }
 
-	private Boolean isCredentialValid(final String credential) {
-		return credential != null && !credential.isEmpty() ? true : false;
-	}
+    private Boolean isUsernameAcknowledged() {
+        return "valid_username".equals(client.readMessageSynchronously());
+    }
 
-	public void addPanels() {
-		add(textAreaScroller, BorderLayout.CENTER);
+    private Boolean isUserNameValid(final String credential) {
+        return credential != null && !credential.isEmpty();
+    }
 
-		add(createUsersListPanel(), BorderLayout.WEST);
+    public void addPanels() {
+        add(textAreaScroller, BorderLayout.CENTER);
 
-		add(createFooterPanel(), BorderLayout.SOUTH);
-	}
+        add(createUsersListPanel(), BorderLayout.WEST);
 
-	public void createListener() {
-		ActionListener sendActionListener = getActionListener();
-		sendButton.addActionListener(sendActionListener);
-		textLine.addActionListener(sendActionListener);
+        add(createFooterPanel(), BorderLayout.SOUTH);
+    }
 
-	}
+    public void createListener() {
+        ActionListener sendActionListener = getActionListener();
+        sendButton.addActionListener(sendActionListener);
+        textLine.addActionListener(sendActionListener);
 
-	private ActionListener getActionListener() {
-		return (e) -> {
-			String message = textLine.getText();
-			client.sendMessage(message);
-			textLine.setText(DEFAULT_TEXTAREA_VALUE);
-		};
-	}
+    }
 
-	private JPanel createFooterPanel() {
-		JPanel footerPanel = new JPanel();
-		footerPanel.setLayout(new BorderLayout());
-		footerPanel.add(textLine, BorderLayout.CENTER);
-		footerPanel.add(sendButton, BorderLayout.EAST);
-		return footerPanel;
-	}
+    private ActionListener getActionListener() {
+        return (e) -> {
+            String message = textLine.getText();
+            client.sendMessage(message);
+            textLine.setText(DEFAULT_TEXTAREA_VALUE);
+        };
+    }
 
-	private JPanel createUsersListPanel() {
-		JList<String> onlineUsersDisplayList = createOnlineUsersDisplayList();
-		JPanel usersListPanel = new JPanel();
-		usersListPanel.setBorder(new TitledBorder(USERS_PANEL_TITLE));
-		usersListPanel.setLayout(new BorderLayout());
-		JScrollPane listScroll = new JScrollPane(onlineUsersDisplayList);
-		listScroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-		usersListPanel.add(listScroll);
-		return usersListPanel;
-	}
+    private JPanel createFooterPanel() {
+        JPanel footerPanel = new JPanel();
+        footerPanel.setLayout(new BorderLayout());
+        footerPanel.add(textLine, BorderLayout.CENTER);
+        footerPanel.add(sendButton, BorderLayout.EAST);
+        return footerPanel;
+    }
 
-	private JList<String> createOnlineUsersDisplayList() {
-		model = new DefaultListModel<>();
-		JList<String> onlineUsersDisplayList = new JList<>(model);
-		onlineUsersDisplayList.setFixedCellWidth(DEFAULT_DISPLAY_LIST_WIDTH);
-		onlineUsersDisplayList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		onlineUsersDisplayList.setFocusable(false);
-		onlineUsersDisplayList.setBorder(
-				new EmptyBorder(TOP_MARGIN_OFFSET, LEFT_MARGIN_OFFSET, BOTTOM_MARGIN_OFFSET, RIGHT_MARGIN_OFFSET));
-		return onlineUsersDisplayList;
-	}
+    private JPanel createUsersListPanel() {
+        JList<String> onlineUsersDisplayList = createOnlineUsersDisplayList();
+        JPanel usersListPanel = new JPanel();
+        usersListPanel.setBorder(new TitledBorder(USERS_PANEL_TITLE));
+        usersListPanel.setLayout(new BorderLayout());
+        JScrollPane listScroll = new JScrollPane(onlineUsersDisplayList);
+        listScroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        usersListPanel.add(listScroll);
+        return usersListPanel;
+    }
 
-	public JScrollPane getTextAreaScroller() {
-		return textAreaScroller;
-	}
+    private JList<String> createOnlineUsersDisplayList() {
+        model = new DefaultListModel<>();
+        JList<String> onlineUsersDisplayList = new JList<>(model);
+        onlineUsersDisplayList.setFixedCellWidth(DEFAULT_DISPLAY_LIST_WIDTH);
+        onlineUsersDisplayList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        onlineUsersDisplayList.setFocusable(false);
+        onlineUsersDisplayList.setBorder(new EmptyBorder(TOP_MARGIN_OFFSET, LEFT_MARGIN_OFFSET, BOTTOM_MARGIN_OFFSET, RIGHT_MARGIN_OFFSET));
+        return onlineUsersDisplayList;
+    }
 
-	public void setTextAreaScroller(JScrollPane textAreaScroller) {
-		this.textAreaScroller = textAreaScroller;
-	}
+    public JScrollPane getTextAreaScroller() {
+        return textAreaScroller;
+    }
 
-	public JButton getSendButton() {
-		return sendButton;
-	}
+    public void setTextAreaScroller(JScrollPane textAreaScroller) {
+        this.textAreaScroller = textAreaScroller;
+    }
 
-	public void setSendButton(JButton sendButton) {
-		this.sendButton = sendButton;
-	}
+    public JButton getSendButton() {
+        return sendButton;
+    }
 
-	public JTextField getTextLine() {
-		return textLine;
-	}
+    public void setSendButton(JButton sendButton) {
+        this.sendButton = sendButton;
+    }
 
-	public void setTextLine(JTextField textLine) {
-		this.textLine = textLine;
-	}
+    public JTextField getTextLine() {
+        return textLine;
+    }
 
-	public JTextArea getChatTextArea() {
-		return chatTextArea;
-	}
+    public void setTextLine(JTextField textLine) {
+        this.textLine = textLine;
+    }
 
-	public void setChatTextArea(JTextArea chatTextArea) {
-		this.chatTextArea = chatTextArea;
-	}
+    public JTextArea getChatTextArea() {
+        return chatTextArea;
+    }
+
+    public void setChatTextArea(JTextArea chatTextArea) {
+        this.chatTextArea = chatTextArea;
+    }
 
 }
